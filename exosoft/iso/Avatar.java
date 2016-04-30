a53b6d770ab4954c817d089c93ede20d0db87004
Campbell Jones <gingerdeadshot@gmail.com>
2016-04-30 15:52:24 -0400

    Keep getting an odd NullPointerException.

--- exosoft/iso/Avatar.java
+++ exosoft/iso/Avatar.java
@@ -6,29 +6,33 @@ public class Avatar extends Sprite implements ObjectPhysics, Controllable {
 
 	public Avatar(SheetType type, String sheetPath, int spriteWidth, int spriteHeight) {
 		super(type, sheetPath, spriteWidth, spriteHeight);
-		bounds = new Rectangle((int) getxPosition(), (int) getyPosition(), spriteWidth, spriteHeight);
+		bounds = new Rectangle((int) getIntxPosition(), getIntyPosition(), spriteWidth, spriteHeight);
 	}
 
 	public void collision() {
-		getBounds().setLocation(getIntxPosition(), getIntyPosition());
-		if (location.checkCollision(getBounds())) {
-			setyVelocity(0);
-			Rectangle intersect = location.intersection(getBounds());
-			while (intersect != new Rectangle()) {
-				try {
-					if (bounds.getMinX() < intersect.getMinX()) {
-						setxPosition(getxPosition() - 1);
+		double nextyPosition = getyPosition() + gravity;
+		Rectangle newBounds = getBounds();
+		newBounds.setLocation(getIntxPosition(), (int) nextyPosition);
+		if (location.checkCollision(newBounds)) {
+			Rectangle intersect = location.intersection(newBounds);
+			try {
+				while (!intersect.isEmpty()) {
+					if (newBounds.getMinX() < intersect.getMinX()) {
+						setxPosition(getIntxPosition() - 1);
 					}
-					if (bounds.getMaxY() > intersect.getMinY()) {
-						setyPosition(getyPosition() - 1);
-						bounds.setLocation((int) getxPosition(), (int) getyPosition());
+					if (newBounds.getMaxY() > intersect.getMinY()) {
+						nextyPosition--;
+						newBounds.setLocation(getIntxPosition(), (int) nextyPosition);
 					}
-					intersect = location.intersection(getBounds());
-				} catch (NullPointerException e) {
-					break;
+					intersect = location.intersection(newBounds);
 				}
+			} catch (NullPointerException e) {
+
 			}
 		}
+
+		setyPosition(nextyPosition);
+		bounds.setLocation(getIntxPosition(), getIntyPosition());
 	}
 
 	public void physics() {

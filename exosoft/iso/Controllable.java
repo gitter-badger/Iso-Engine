43c48bada14674e9895800a5dfe54fa62a411915
Campbell Jones <gingerdeadshot@gmail.com>
2016-05-06 07:10:15 -0400

    hyep

--- exosoft/iso/Controllable.java
+++
@@ -1,9 +0,0 @@
-package exosoft.iso;
-
-public interface Controllable {
-	public void moveLeft(double multiplier);
-	public void moveRight(double multiplier);
-	public void moveUp(double multiplier);
-	public void moveDown(double multiplier);
-	public void movement();
-}

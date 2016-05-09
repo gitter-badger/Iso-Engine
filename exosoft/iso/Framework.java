13c3224d95d468b4aa78dfcb7c51ef0a8974b0cc
Campbell Jones <gingerdeadshot@gmail.com>
2016-05-09 09:06:56 -0400

    Framework Patch 1.1
    Added KeyListener to basic variable set.
    
    
    
    Added JPanel to basic variable set.
    
    
    
    Added Environment to basic variable set.

--- exosoft/iso/Framework.java
+++ exosoft/iso/Framework.java
@@ -3,6 +3,9 @@ package exosoft.iso;
 import javax.swing.Timer;
 
 public class Framework {
+	protected static Environment map;
+	protected static JPanel sheet;
+	protected static KeyListener keywatch;
 	protected static int logicRate;
 	protected static int drawRate;
 	protected static volatile Timer drawTimer;
